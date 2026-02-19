79d6f7205ecea4b05a23684ceb3bf29d1507778d
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2026-02-19 11:53:31 -0500

    Disable SSL warnings

--- atoto_fw/core/http.py
+++ atoto_fw/core/http.py
@@ -1,5 +1,9 @@
 import requests
 from requests.adapters import HTTPAdapter, Retry
+import urllib3
+
+# Disable SSL warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 UA   = "ATOTO-Firmware-CLI/3.2"
 
@@ -14,6 +18,7 @@ def make_session() -> requests.Session:
     s = requests.Session()
     s.mount("http://", adapter); s.mount("https://", adapter)
     s.headers.update({"User-Agent": UA})
+    s.verify = False  # Disable SSL certificate verification
     return s
 
 SESSION = make_session()
